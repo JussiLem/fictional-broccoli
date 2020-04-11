4d6c6e717a8cca5421fa47fc216c6e788a319f8a
Jussi Lemmetyinen <jussi.lemmetyinen@siili.com>
2020-04-11 13:21:57 +0300

    Removed console.log call

--- exercise5/components/History.js
+++ exercise5/components/History.js
@@ -3,7 +3,6 @@ import {SafeAreaView, Text, FlatList, StyleSheet} from 'react-native';
 
 export default ({ route, navigation }) => {
   const { history } = route.params
-  console.log(JSON.stringify(route.params))
   return (
     <SafeAreaView style={styles.history}>
       <Text>History</Text>
